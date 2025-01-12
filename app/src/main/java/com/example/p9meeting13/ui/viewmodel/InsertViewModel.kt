c6b1f47ff04cebb4aa6b5fb5e9c960b51eefeae3
mrendy58 <derendyyy58@gmail.com>
2025-01-12 18:08:27 +0700

    membuat fungsi untuk menyimpat data dari MahasiswaEvent ke mahasiswa

--- app/src/main/java/com/example/p9meeting13/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p9meeting13/ui/viewmodel/InsertViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.p9meeting13.ui.viewmodel
 
+import com.example.p9meeting13.model.Mahasiswa
+
 class InsertViewModel {
 }
+
+//menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
 
 // data class untuk menyimpan data input form
 data class MahasiswaEvent(
