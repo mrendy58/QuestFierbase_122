53cd1d94b9c19ef90008bd5ad30c0b71f8b0d778
mrendy58 <derendyyy58@gmail.com>
2025-01-12 17:45:47 +0700

    membuat fungsi deleteMhs di HomeViewModel

--- app/src/main/java/com/example/p9meeting13/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/p9meeting13/ui/viewmodel/HomeViewModel.kt
@@ -38,6 +38,15 @@ class HomeViewModel (
                 }
         }
     }
+    fun deleteMhs(mahasiswa: Mahasiswa){
+        viewModelScope.launch {
+            try {
+                repoMhs.deleteMhs(mahasiswa)
+            } catch (e: Exception){
+                mhsUiState = HomeUiState.Error(e)
+            }
+        }
+    }
 }
 
 sealed class HomeUiState{
