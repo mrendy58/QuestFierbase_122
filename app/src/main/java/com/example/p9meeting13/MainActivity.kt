5e64f02d82c6a79b9b05b715783d44f3836d618d
mrendy58 <derendyyy58@gmail.com>
2025-01-13 00:12:20 +0700

    memperbaharui navigasi di MainActivity

--- app/src/main/java/com/example/p9meeting13/MainActivity.kt
+++ app/src/main/java/com/example/p9meeting13/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.p9meeting13.ui.navigation.PengelolaHalaman
 import com.example.p9meeting13.ui.theme.P9Meeting13Theme
 import com.example.p9meeting13.ui.view.HomeScreen
 
@@ -21,8 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             P9Meeting13Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(
-                        navigateToItemEntry = {},
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
